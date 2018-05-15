fcbc1718649c9057d33dd044b574b4d761cc5f27
Sergi Reyner <sergi.reyner@gmail.com>
2018-05-15 01:37:08 +0100

    The gameover screen displays how many coins were collected

--- game-over.scm
+++ game-over.scm
@@ -1,18 +1,17 @@
 ;; --------------------
 ;; game-over state
 (define restart-button #f)
+(define death-text #f)
 
 (define (game-over-draw)
   (let* ((w (glgui-width-get))
          (h (glgui-height-get)))
-    ;; game-over screen
-    ;;  title
-    ;;  restart button
-    (set! restart-button (glgui-button-string gui 200 360 100 20 "START" calc_14.fnt
-                                            (lambda args
-                                              (game-over-end)
-                                              (game-draw)
-                                              (set! handle-fn game-handle))))))
+    (set! death-text (glgui-label gui 200 400 100 20 (number->string (slot-ref *hero* 'coins)) calc_14.fnt White))
+    (set! restart-button (glgui-button-string gui 140 240 120 40 "OK" calc_24.fnt
+                                              (lambda args
+                                                (game-over-end)
+                                                (menu-draw)
+                                                (set! handle-fn menu-handle))))))
 
 (define (game-over-handle t x y)
   (let ((skipevent #f))
@@ -22,4 +21,5 @@
     (if (not skipevent) (glgui-event gui t x y))))
 
 (define (game-over-end)
-  (glgui-widget-delete gui restart-button))
+  (glgui-widget-delete gui restart-button)
+  (glgui-widget-delete gui death-text))
