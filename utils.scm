445d22e7061f4992779c883ee95a264815a32477
Sergi Reyner <sergi.reyner@gmail.com>
2018-05-20 19:01:04 +0100

    Apparently I forgot to commit the utils file...?

--- utils.scm
+++ utils.scm
@@ -3,3 +3,23 @@
     (if x (cadr x) '())))
 
 (define (random-elt l) (list-ref l (random-integer (length l))))
+
+(define (lerp a b t) (+ (* (- 1 t) a) (* t b)))
+
+(define (nth n l)
+  (if (or (>= n (length l)) (< n 0))
+      (error "Index out of bounds.")
+      (if (eq? n 0)
+          (car l)
+          (nth (- n 1) (cdr l)))))
+
+;; this does foldr
+;; (define (reduce fn init list)
+;;   (if (null? list)
+;;       init
+;;       (fn (car list) (reduce fn init (cdr list)))))
+
+(define (reduce fn init list)
+  (if (null? list)
+      init
+      (reduce fn (fn init (car list)) (cdr list))))
